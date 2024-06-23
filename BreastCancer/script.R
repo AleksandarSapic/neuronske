28f5d6393b88c1e34bd82a9f77390c845a7709ba
AleksandarSapic <shapke10@gmail.com>
2024-06-23 19:32:09 +0200

    Corrected example

--- BreastCancer/script.R
+++ BreastCancer/script.R
@@ -15,74 +15,55 @@ data.std$X1 = data$X1
 
 #Mesanje redova u data setu
 set.seed(123)
-data.std <- data.std[sample(1:nrow(data.std)),]
+data.std <- data.std[sample(1:nrow(data.std)), ]
 
 #Kreiranje trening i test seta
 library(caret)
 set.seed(123)
 train.indicies <-
   createDataPartition(data.std$X1, p = 0.65, list = FALSE)
-train.data <- data.std[train.indicies,]
-test.data <- data.std[-train.indicies, ]
+train.data <- data.std[train.indicies, ]
+test.data <- data.std[-train.indicies,]
 
 #Treniranje neuronske mreze
 library(neuralnet)
 
-#Prva mreza
-#Learning rate 0.2, broj neurona 10
-nn1 <- neuralnet(
-  X1 ~ .,
-  data = train.data,
-  err.fct = 'ce',
-  hidden = 10,
-  learningrate = 0.2,
-  linear.output = FALSE
-)
+learning_rates <- c(0.2, 0.4, 0.6)
+hidden_nodes <- c(10, 20, 30)
 
-#Pravljenje predikcije
-nn1.predictions <- predict(nn1, newdata = test.data)
-#Racunanje srednje kvadratne greske
-nn1.mse <- mean((test.data$X1 - nn1.predictions) ^ 2)
+models <- list()
+predictions <- list()
+mses <- data.frame()
+iteration <- 1
 
+for (lr in learning_rates) {
+  for (n in hidden_nodes) {
+    nn <- neuralnet(
+      X1 ~ .,
+      data = train.data,
+      err.fct = 'ce',
+      hidden = n,
+      learningrate = lr,
+      linear.output = FALSE
+    )
+    prediction <- predict(nn, newdata = test.data)
+    mse <- mean((test.data$X1 - prediction) ^ 2)
+    
+    models[[paste("lr", lr, "hidden", n, sep = "_")]] <- nn
+    predictions[[paste("lr", lr, "hidden", n, sep = "_")]] <-
+      prediction
+    mses <- rbind(mses, c(iteration, mse))
+    iteration <- iteration + 1
+  }
+}
 
+print(mses)
+colnames(mses) <- c('Iteration', 'MSE')
+mses[order(mses$MSE, decreasing = FALSE), ]
 
-#Druga mreza
-#Learning rate 0.4, broj neurona 20
-nn2 <- neuralnet(
-  X1 ~ .,
-  data = train.data,
-  err.fct = 'ce',
-  hidden = 20,
-  learningrate = 0.4,
-  linear.output = FALSE
-)
+#Mreza sa najmanjom greskom je mreza iz iteracije 9
+#Sa learning rateom 0.6 i nodes 30
+print(models$lr_0.6_hidden_30)
 
-#Pravljenje predikcije
-nn2.predictions <- predict(nn2, newdata = test.data)
-#Racunanje srednje kvadratne greske
-nn2.mse <- mean((test.data$X1 - nn2.predictions) ^ 2)
-
-
-#Treca mreza
-#Learning rate 0.6, broj neurona 30
-nn3 <- neuralnet(
-  X1 ~ .,
-  data = train.data,
-  err.fct = 'ce',
-  hidden = 30,
-  learningrate = 0.6,
-  linear.output = FALSE
-)
-
-#Pravljenje predikcije
-nn3.predictions <- predict(nn3, newdata = test.data)
-#Racunanje srednje kvadratne greske
-nn3.mse <- mean((test.data$X1 - nn3.predictions) ^ 2)
-
-
-mse.compare = rbind(c('NN1', 'NN2', 'NN3'), c(nn1.mse, nn2.mse, nn3.mse))
-mse.compare
-
-#Mreza sa najmanjom greskom je NN2
 #Cuvanje mreze u fajl
-saveRDS(nn2, file = 'neural_net_model.rds')
+saveRDS(models$lr_0.6_hidden_30, file = 'neural_net_model.rds')
